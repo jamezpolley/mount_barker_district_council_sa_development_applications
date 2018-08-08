68ea7012d5cca989e87f3edc02b3f86706f35401
MichaelBone <Michael.Bone@senet.com.au>
2018-08-08 21:18:10 +0930

    Initial implementation.

--- scraper.ts
+++ scraper.ts
@@ -3,18 +3,25 @@
 //
 // Michael Bone
 // 8th August 2018
+
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cheerio = require("cheerio");
-const request = require("request-promise-native");
-const sqlite3 = require("sqlite3");
-const pdf2json = require("pdf2json");
-const urlparser = require("url");
-const moment = require("moment");
+
+import * as cheerio from "cheerio";
+import * as request from "request-promise-native";
+import * as sqlite3 from "sqlite3";
+import * as pdf2json from "pdf2json";
+import * as urlparser from "url";
+import * as moment from "moment";
+
 sqlite3.verbose();
+
 const DevelopmentApplicationsUrl = "https://www.mountbarker.sa.gov.au/developmentregister";
 const CommentUrl = "mailto:council@mountbarker.sa.gov.au";
+
+declare const global: any;
+
 // Sets up an sqlite database.
+
 async function initializeDatabase() {
     return new Promise((resolve, reject) => {
         let database = new sqlite3.Database("data.sqlite");
@@ -24,7 +31,9 @@ async function initializeDatabase() {
         });
     });
 }
+
 // Inserts a row in the database if it does not already exist.
+
 async function insertRow(database, developmentApplication) {
     return new Promise((resolve, reject) => {
         let sqlStatement = database.prepare("insert or ignore into [data] values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
@@ -38,105 +47,119 @@ async function insertRow(database, developmentApplication) {
             developmentApplication.receivedDate,
             null,
             null
-        ], function (error, row) {
+        ], function(error, row) {
             if (error) {
                 console.error(error);
                 reject(error);
-            }
-            else {
+            } else {
                 if (this.changes > 0)
                     console.log(`    Inserted: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\" and reason \"${developmentApplication.reason}\" into the database.`);
                 else
                     console.log(`    Skipped: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\" and reason \"${developmentApplication.reason}\" because it was already present in the database.`);
-                sqlStatement.finalize(); // releases any locks
+                sqlStatement.finalize();  // releases any locks
                 resolve(row);
             }
         });
     });
 }
+
 // Parses the development applications.
+
 async function main() {
     // Ensure that the database exists.
+
     let database = await initializeDatabase();
+
     // Retrieve the page contains the links to the PDFs.
+
     console.log(`Retrieving page: ${DevelopmentApplicationsUrl}`);
     let body = await request(DevelopmentApplicationsUrl);
     let $ = cheerio.load(body);
-    let pdfUrls = [];
+
+    let pdfUrls: string[] = [];
     for (let element of $("td.uContentListDesc a[href$='.pdf']").get()) {
         let pdfUrl = new urlparser.URL(element.attribs.href, DevelopmentApplicationsUrl).href;
         if (!pdfUrls.some(url => url === pdfUrl))
             pdfUrls.push(pdfUrl);
     }
+
     if (pdfUrls.length === 0) {
         console.log("No PDF URLs were found on the page.");
         return;
     }
+
     // Select the most recent PDF.  And randomly select one other PDF (avoid processing all PDFs
     // at once because this may use too much memory, resulting in morph.io terminating the current
     // process).
-    let selectedPdfUrls = [];
+
+    let selectedPdfUrls: string[] = [];
     selectedPdfUrls.push(pdfUrls.shift());
     if (pdfUrls.length > 0)
         selectedPdfUrls.push(pdfUrls[getRandom(1, pdfUrls.length)]);
+
     for (let pdfUrl of selectedPdfUrls) {
         console.log(`Retrieving document: ${pdfUrl}`);
+
         // Parse the PDF into a collection of PDF rows.  Each PDF row is simply an array of
         // strings, being the text that has been parsed from the PDF.
+
         let pdfParser = new pdf2json();
         let pdfPipe = request({ url: pdfUrl, encoding: null }).pipe(pdfParser);
         pdfPipe.on("pdfParser_dataError", error => {
             console.log("In pdfParser_dataError catch.");
             console.log(error);
         });
-        pdfPipe.on("pdfParser_dataReady", async (pdf) => {
+        pdfPipe.on("pdfParser_dataReady", async pdf => {
             try {
                 // Convert the JSON representation of the PDF into a collection of PDF rows.
+
                 console.log(`Parsing document: ${pdfUrl}`);
                 let rows = convertPdfToText(pdf);
+
                 let developmentApplications = [];
                 let developmentApplication = null;
                 let isAddress = false;
+
                 for (let row of rows) {
                     let text = (row.length === 0) ? "" : row[0].trim().toLowerCase();
+
                     // The start of an application is indicated by the text "Dev App No".
+
                     if (text.startsWith("dev app no") && row.length >= 3) {
                         developmentApplication = {
                             applicationNumber: row[1].trim().replace(/\s/g, ""),
                             address: "",
                             reason: row[2].trim(),
-                            informationUrl: pdfUrl,
+                            informationUrl : pdfUrl,
                             commentUrl: CommentUrl,
-                            scrapeDate: moment().format("YYYY-MM-DD"),
+                            scrapeDate : moment().format("YYYY-MM-DD"),
                             receivedDate: ""
-                        };
+                        }
                         developmentApplications.push(developmentApplication);
-                    }
-                    else if (developmentApplication !== null) {
+                    } else if (developmentApplication !== null) {
                         // Watch for the start of an address (based on encountering the heading
                         // "Property Detail").
+
                         if (!isAddress && text.startsWith("property detail")) {
                             isAddress = true;
-                        }
-                        else if (isAddress && (text.trim() === "" || text.startsWith("fees"))) {
+                        } else if (isAddress && (text.trim() === "" || text.startsWith("fees"))) {
                             isAddress = false;
                             developmentApplication = null;
-                        }
-                        else if (isAddress && text.startsWith("property detail") && row.length >= 2) {
+                        } else if (isAddress && text.startsWith("property detail") && row.length >= 2) {
                             developmentApplication.address += ((developmentApplication.address === "") ? "" : " ") + row[1].trim();
-                        }
-                        else if (isAddress && row.length >= 1 && (row[0].trim().startsWith("LOT:") || row[0].trim().startsWith("ALT:") || row[0].trim().startsWith("A:") || row[0].trim().startsWith("U:") || row[0].trim().startsWith("PCE:") || row[0].trim().startsWith("SEC:"))) {
+                        } else if (isAddress && row.length >= 1 && (row[0].trim().startsWith("LOT:") || row[0].trim().startsWith("ALT:") || row[0].trim().startsWith("A:") || row[0].trim().startsWith("U:") || row[0].trim().startsWith("PCE:") || row[0].trim().startsWith("SEC:"))) {
                             isAddress = false;
                             developmentApplication = null;
-                        }
-                        else if (isAddress && row.length >= 1) {
+                        } else if (isAddress && row.length >= 1) {
                             developmentApplication.address += ((developmentApplication.address === "") ? "" : " ") + row[0].trim();
                         }
+
                         // Attempt to find "Application Rec'd Council" anywhere on the row.
+
                         if (developmentApplication !== null && developmentApplication.receivedDate === "") {
                             for (let index = 0; index < row.length; index++) {
                                 if (row[index].trim().toLowerCase().startsWith("application rec'd council") && index + 1 < row.length) {
-                                    let receivedDate = moment(row[index + 1].trim(), "D/MM/YYYY", true); // allows the leading zero of the day to be omitted
+                                    let receivedDate = moment(row[index + 1].trim(), "D/MM/YYYY", true);  // allows the leading zero of the day to be omitted
                                     if (receivedDate.isValid()) {
                                         developmentApplication.receivedDate = receivedDate.format("YYYY-MM-DD");
                                         break;
@@ -146,25 +169,31 @@ async function main() {
                         }
                     }
                 }
+
                 for (let developmentApplication of developmentApplications)
                     await insertRow(database, developmentApplication);
+
                 console.log(`Parsed document: ${pdfUrl}`);
+
                 // Attempt to avoid reaching 512 MB memory usage (this will otherwise result in
                 // the current process being terminated by morph.io).
+
                 if (global.gc)
                     global.gc();
-            }
-            catch (ex) {
+            } catch (ex) {
                 console.log("In pdfParser_dataReady catch.");
                 console.log(ex);
             }
         });
     }
 }
+
 // Gets a random integer in the specified range: [minimum, maximum).
-function getRandom(minimum, maximum) {
+
+function getRandom(minimum: number, maximum: number) {
     return Math.floor(Math.random() * (Math.floor(maximum) - Math.ceil(minimum))) + Math.ceil(minimum);
 }
+
 // Convert a parsed PDF into an array of rows.  This function is based on pdf2table by Sam Decrock.
 // See https://github.com/SamDecrock/pdf2table/blob/master/lib/pdf2table.js.
 //
@@ -189,22 +218,29 @@ function getRandom(minimum, maximum) {
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+
 function convertPdfToText(pdf) {
     let xComparer = (a, b) => (a.x > b.x) ? 1 : ((a.x < b.x) ? -1 : 0);
     let yComparer = (a, b) => (a.y > b.y) ? 1 : ((a.y < b.y) ? -1 : 0);
+
     // Find the smallest Y co-ordinate for two texts with equal X co-ordinates.
+
     let smallestYValueForPage = [];
+
     for (let pageIndex = 0; pageIndex < pdf.formImage.Pages.length; pageIndex++) {
         let page = pdf.formImage.Pages[pageIndex];
-        let smallestYValue = null; // per page
+        let smallestYValue = null;  // per page
         let textsWithSameXValues = {};
+
         for (let textIndex = 0; textIndex < page.Texts.length; textIndex++) {
             let text = page.Texts[textIndex];
             if (!textsWithSameXValues[text.x])
                 textsWithSameXValues[text.x] = [];
             textsWithSameXValues[text.x].push(text);
         }
+
         // Find smallest Y distance.
+
         for (let x in textsWithSameXValues) {
             let texts = textsWithSameXValues[x];
             for (let i = 0; i < texts.length; i++) {
@@ -214,68 +250,85 @@ function convertPdfToText(pdf) {
                         if (smallestYValue === null || distance < smallestYValue)
                             smallestYValue = distance;
                     }
-                }
-                ;
-            }
-            ;
+                };
+            };
         }
+
         if (smallestYValue === null)
             smallestYValue = 0;
         smallestYValueForPage.push(smallestYValue);
     }
+
     // Find texts with similar Y values (in the range of Y - smallestYValue to Y + smallestYValue).
+
     let myPages = [];
+
     for (let pageIndex = 0; pageIndex < pdf.formImage.Pages.length; pageIndex++) {
         let page = pdf.formImage.Pages[pageIndex];
-        let rows = []; // store texts and their X positions in rows
+
+        let rows = [];  // store texts and their X positions in rows
+
         for (let textIndex = 0; textIndex < page.Texts.length; textIndex++) {
             let text = page.Texts[textIndex];
+
             let foundRow = false;
             for (let rowIndex = rows.length - 1; rowIndex >= 0; rowIndex--) {
                 // Y value of text falls within the Y value range, add text to row.
+
                 let maximumYdifference = smallestYValueForPage[pageIndex];
                 if (rows[rowIndex].y - maximumYdifference < text.y && text.y < rows[rowIndex].y + maximumYdifference) {
                     // Only add value of T to data (which is the actual text).
+
                     for (let index = 0; index < text.R.length; index++)
                         rows[rowIndex].data.push({ text: decodeURIComponent(text.R[index].T), x: text.x });
                     foundRow = true;
                 }
-            }
-            ;
+            };
+
             // Create a new row and add the text to the row.
+
             if (!foundRow) {
                 let row = { y: text.y, data: [] };
                 for (let index = 0; index < text.R.length; index++)
                     row.data.push({ text: decodeURIComponent(text.R[index].T), x: text.x });
                 rows.push(row);
             }
-        }
-        ;
+        };
+
         // Sort each extracted row horizontally by X co-ordinate.
+
         for (let index = 0; index < rows.length; index++)
             rows[index].data.sort(xComparer);
+
         // Sort rows vertically by Y co-ordinate.
+
         rows.sort(yComparer);
+
         // Add rows to pages.
+
         myPages.push(rows);
-    }
-    ;
+    };
+
     // Flatten pages into rows.
+
     let rows = [];
+
     for (let pageIndex = 0; pageIndex < myPages.length; pageIndex++) {
         for (let rowIndex = 0; rowIndex < myPages[pageIndex].length; rowIndex++) {
             // Now that each row is made of objects extract the text property from the object.
-            let rowEntries = [];
+
+            let rowEntries = []
             let row = myPages[pageIndex][rowIndex].data;
             for (let index = 0; index < row.length; index++)
                 rowEntries.push(row[index].text);
+
             // Append the extracted and ordered text into the return rows.
+
             rows.push(rowEntries);
-        }
-        ;
-    }
-    ;
+        };
+    };
+
     return rows;
 }
+
 main().catch(error => console.error(error));
-//# sourceMappingURL=scraper.js.map
