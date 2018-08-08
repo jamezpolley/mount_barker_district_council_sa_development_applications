e5aee82e30047126fec9416c4df92e2e8c41e10a
MichaelBone <Michael.Bone@senet.com.au>
2018-08-08 21:38:11 +0930

    Changed to a different protocol.

--- scraper.ts
+++ scraper.ts
@@ -73,7 +73,7 @@ async function main() {
     // Retrieve the page contains the links to the PDFs.
 
     console.log(`Retrieving page: ${DevelopmentApplicationsUrl}`);
-    let body = await request({ url: DevelopmentApplicationsUrl, agentOptions: { secureProtocol: "SSLv3_method" } });
+    let body = await request({ url: DevelopmentApplicationsUrl, agentOptions: { secureProtocol: "TLSV1_1_method" } });
     let $ = cheerio.load(body);
 
     let pdfUrls: string[] = [];
